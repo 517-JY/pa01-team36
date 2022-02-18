3ad149ee0dd7cee14c85eb670a4eb11f49082eaf
Jingnu An <ajingnv@gmail.com>
2022-02-17 20:35:57 -0500

    delete print for description

--- course_search.py
+++ course_search.py
@@ -83,7 +83,6 @@ def print_course(course):
     '''print_course prints a brief description of the course '''
     print(course['subject'],course['coursenum'],course['section'],
           course['name'],course['term'],course['instructor'])
-    print('-- course description --\n', course['description'], '\n')
 
 if __name__ == '__main__':
     topmenu()
